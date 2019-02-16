90b759c1bce25d31d3781446d3e3ba43f4d37607
gunapmk <gunapmk@gmail.com>
2019-02-16 20:39:11 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
-
-\import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
